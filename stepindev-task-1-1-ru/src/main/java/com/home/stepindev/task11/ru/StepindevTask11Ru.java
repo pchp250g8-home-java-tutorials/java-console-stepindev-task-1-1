9b3cf3600467f1549d79b989692aea90dc8180f9
Alexander <pchp250g8@tutamail.com>
2026-08-30 20:49:13 +0300

    Created Java Repositary Task-1-1

---
+++ stepindev-task-1-1-ru/src/main/java/com/home/stepindev/task11/ru/StepindevTask11Ru.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.home.stepindev.task11.ru;
+import java.io.*;
+
+/**
+ *
+ * @author PC
+ */
+public class StepindevTask11Ru 
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        var oStdIn = new BufferedReader(new InputStreamReader(System.in,"utf-8"));
+        var oStdOut = new PrintWriter(new OutputStreamWriter(System.out,"utf-8"),true);
+        oStdOut.println("Введите количество блоков");
+        var n = Integer.parseInt(oStdIn.readLine());
+        oStdOut.println("Введите количество башен");
+        var k = Integer.parseInt(oStdIn.readLine());
+        var t = n / k; // Максимальное количество башен
+        var b = t * k; // Количесво блоков
+        oStdOut.printf
+        (
+            "Из %d блоков можно построить максимум %d башен(ни)\r\n",
+            n,t
+        );
+        oStdOut.printf("На это уйдёт %d блоков.\r\n",b);
+    }
+}
