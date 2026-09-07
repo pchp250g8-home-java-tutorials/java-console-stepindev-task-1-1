2d20ff118963e15ef34a5ed7fab926e78c99901b
Alexander <pchp250g8@tutamail.com>
2026-09-07 12:37:10 +0300

    Reorganized Solution Structure

---
+++ task-1-1-en/src/main/java/com/home/stepindev/task11/Program.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.home.stepindev.task11;
+import java.io.*;
+/**
+ *
+ * @author PC
+ */
+public class Program 
+{
+    public static void main(String[] args) throws Exception
+    {
+        // Input Data
+        var oStdIn = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number of blocks");
+        var n = Integer.parseInt(oStdIn.readLine());
+        System.out.println("Enter the number of towers");
+        var k = Integer.parseInt(oStdIn.readLine());
+        var t = n / k; // Maximum number of towers
+        var b = t * k; // Number of blocks
+        // Print information to display
+        System.out.printf
+        (
+            "From %d blocks, you can build a maximum of %d towers\r\n",
+            n,t
+        );
+        System.out.printf("This will take %d blocks.\r\n",b);
+    }
+}
---
+++ task-1-1-ru/src/main/java/com/home/stepindev/task11/Program.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.home.stepindev.task11;
+import java.io.*;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author PC
+ */
+public class Program
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        /*Ввод данных*/
+        var strCodeName = Charset.defaultCharset().displayName();
+        var oStdIn = new BufferedReader(new InputStreamReader(System.in, strCodeName));
+        var oStdOut = new PrintWriter(new OutputStreamWriter(System.out,strCodeName),true);
+        oStdOut.println("Введите количество блоков");
+        var n = Integer.parseInt(oStdIn.readLine());
+        oStdOut.println("Введите количество блоков на башню");
+        var k = Integer.parseInt(oStdIn.readLine());
+        var t = n / k; // Максимальное количество башен
+        var b = t * k; // Количесво блоков
+        /*Вывод информации на экран*/
+        oStdOut.printf
+        (
+            "Из %d блоков можно построить максимум %d башен(ни)\r\n",
+            n,t
+        );
+        oStdOut.printf("На это уйдёт %d блоков.\r\n",b);
+    }
+}
